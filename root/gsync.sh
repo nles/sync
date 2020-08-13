e01b1c7790f72663a3cbc8c984e9e7e32cbb5b4e
Nikke Leskelä <nileskel@gmail.com>
2020-08-13 09:53:49 +0300

    Update gsync.sh to remove hanged up processes.

--- root/gsync.sh
+++ root/gsync.sh
@@ -14,3 +14,16 @@ if [ -f /usr/local/bin/grive2 ]; then
 else
   /usr/bin/grive2 ${@:1}
 fi
+
+# kill old process (grive hangs up sporadically...)
+OUTPUT=$(ps aux | grep grive | grep -v grep | awk '{print $2}')
+OUTPUTARRAY=(${OUTPUT})
+echo "TOTAL OLD PROCESSES $(ps aux | grep grive | grep -v grep | wc -l)"
+for PID in "${OUTPUTARRAY[@]}"; do
+  ETIME=$(ps -p "$PID" -o etimes=)
+  if [ $ETIME -gt $((30*60)) ] ; then
+    kill $PID
+  fi
+done
+
+echo ""
